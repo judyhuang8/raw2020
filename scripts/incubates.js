36f108c7db4d424ca69a0924650fedaaa1d76363
Cindy Huang <ch832@cornell.edu>
2020-05-24 12:56:02 -0400

    animated hover effect and x icon

--- scripts/incubates.js
+++ scripts/incubates.js
@@ -46,7 +46,7 @@ $(document).ready(function () {
   $("#incubates-nav-small-1").on("click", function () {
     $("#title").html("Heart-Tube-Heart: Designing a “connector” for story-telling and unexpected bonding");
     $("#authors").html("Sheri Guo, Connie Liu, Benji Magin, Mikail Maqsood, Joseph Yoon");
-    $("#incubates-blob-img").html('<img id="incubates-img" src="images/incubates/Mask Group.png" onclick="showContent()"></img>');
+    $("#incubates-blob-img").html('<div class="square"><span></span><span></span><span></span><div class="content"><img id="incubates-img" src="images/incubates/blob-img-1.png" onclick="showContent()"></div>');
     $("#incubates-img-full").html('<img src="images/incubates/full-img-1.jpg">');
     $("#description").html('Heart-Tube-heart is an installation intended to be located on the Milstein Hall walkway at Cornell University for Medium RAW Expo. The installation consists of multiple sets of height adjustable pipes that loops over the walkway. People can interact with each other through the pipe without being able to directly see each other, removing any social or visual preconceptions. People are able to press buttons that correspond to various questions that serve as conversation starters, facilitating storytelling and helping people to quickly build deeper connections with others.');
     $("#nav-path-1").attr("fill", "black");
@@ -58,7 +58,7 @@ $(document).ready(function () {
   $("#incubates-nav-small-2").on("click", function () {
     $("#title").html("No-Body: Redesigning Restroom Signage");
     $("#authors").html("Lauren Lam, Hahnbee Lee");
-    $("#incubates-blob-img").html('<img id="incubates-img" src="images/incubates/blob-img-2.png" onclick="showContent()"></img>');
+    $("#incubates-blob-img").html('<div class="square"><span></span><span></span><span></span><div class="content"><img id="incubates-img" src="images/incubates/blob-img-2.png" onclick="showContent()"></div>');
     $("#incubates-img-full").html('<img src="images/incubates/full-img-2.png">');
     $("#description").html('We met up at oishii bowl to discuss ideas about “diversity and inclusion”. In the midst of the conversation, Lauren suddenly had to use the bathroom. She noticed the gender-neutral bathroom sign and thought that it wasn’t inclusive and overall a poor design. That’s when we decided to redesign bathroom signs. Bathroom signs are so common that they are usually overlooked, but in reality they reveal how people are categorized. During the Jim Crow era, they were used to segregate black and white people. Today, they are widely used to separate people by the gender binary: men and women. Because bathroom signs are representations for how society categorizes people, it is important that the signs are inclusive.');
     $("#nav-path-2").attr("fill", "black");
@@ -70,7 +70,7 @@ $(document).ready(function () {
   $("#incubates-nav-small-3").on("click", function () {
     $("#title").html("Fibers: Designing an Inclusive Clothing Community Platform");
     $("#authors").html("Erika Hauschild, Fiona Rath<br>With help from Chelsea choi, Sarah Mirae Kim, Stephanie Zheng");
-    $("#incubates-blob-img").html('<img id="incubates-img" src="images/incubates/blob-img-3.png" onclick="showContent()"></img>');
+    $("#incubates-blob-img").html('<div class="square"><span></span><span></span><span></span><div class="content"><img id="incubates-img" src="images/incubates/blob-img-3.png" onclick="showContent()"></div>');
     $("#incubates-img-full").html('<img src="images/incubates/full-img-3.png">');
     $("#description").html('Placeholder: We focused on helping those with disabilities find stylish clothing that suits their needs. For our first iteration we had designed a search engine that would simply help users find adaptive clothing brands. However, we later realized that there are only a limited number of adaptive clothing brands available. Additionally, the adaptive clothing brands available are usually not stylish, and the several stylish brands like Tommy Hilfiger are not affordable for most people and have a limited selection of clothing.');
     $("#nav-path-3").attr("fill", "black");
@@ -82,7 +82,7 @@ $(document).ready(function () {
   $("#incubates-nav-small-4").on("click", function () {
     $("#title").html("FVSO: Fact and Opinion Checker");
     $("#authors").html("Olivia Gee, Varun Iyengar, Iris Kim, Ami Mehta, Valentin Porcellini");
-    $("#incubates-blob-img").html('<img id="incubates-img" src="images/incubates/blob-img-4.png" onclick="showContent()"></img>');
+    $("#incubates-blob-img").html('<div class="square"><span></span><span></span><span></span><div class="content"><img id="incubates-img" src="images/incubates/blob-img-4.png" onclick="showContent()"></div>');
     $("#incubates-img-full").html('<img src="images/incubates/full-img-4.jpg">');
     $("#description").html('Placeholder: Our solution centers on headline indexing and analysis. Roy Clark of the Poynter Institute once stated “in an ideological war, one side or another will try to use language as a weapon.” Evidently, buzzwords and loaded language are powerful tools that shape public opinion.');
     $("#nav-path-4").attr("fill", "black");

1a02ad942aae404f43fbcfbca15582d6d3370300
Judy Huang <jh994@cornell.edu>
2020-05-29 14:53:40 -0400

    changed coutdown time

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("June 1, 2020 12:00:00").getTime();
+var countDownDate = new Date("June 1, 2020 15:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
